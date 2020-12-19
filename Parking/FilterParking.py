0499f0e85a9215ffb70f508c0eb2e3e178819314
bikeoid <niceman@att.net>
2020-12-19 12:35:08 -0500

    Add parking lots and data conversion

---
+++ Parking/FilterParking.py
@@ -0,0 +1,26 @@
+'''
+A translation function for Greenville city shp parking data . 
+
+The following fields are used:    
+
+Field           Used For            Reason
+FEAT_CODE          -
+
+
+
+Constant fields:
+
+
+'''
+
+import re
+
+    
+def filterTags(attrs):
+    if not attrs:
+        return
+
+    tags = {}
+    
+
+    return tags
